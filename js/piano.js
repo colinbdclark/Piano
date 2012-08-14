8949c509eecb2ef25bc47b5972b4bcbcd988f63d
Myles Borins <myles.borins@gmail.com>
2012-08-14 03:19:57 -0400

    Title working

--- js/piano.js
+++ js/piano.js
@@ -132,13 +132,16 @@ var automm = automm || {};
             svg.attr("style", "height: 100%;");
             svg.attr("viewBox", that.model.viewbox.dim);
             svg.attr("role", "application");
+            svg.attr("focusable", true);
+            svg.attr("tabindex", "0");
+            svg.attr("aria-labelledby", "ariaTitle");
             
+
             that.noteGroup = svg.append("g");
             that.noteGroup.attr("transform", "translate(" + that.model.padding / 2 + "," + that.model.padding / 2 + ")");
             that.noteGroup.attr("role", "controlgroup");
             that.noteGroup.attr("id", "noteGroup");
             that.noteGroup.attr("focusable", true);
-
             // Draw the keys
             that.render();
         };
